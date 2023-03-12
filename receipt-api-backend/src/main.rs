d7ac46bdf685bde09effa07bdd89cc40f1b7d3eb
Tylor Hanshaw <tylorjhanshaw@gmail.com>
2023-03-12 15:55:07 -0400

    playing with paramater guards in endpoint paths with Rocket

--- receipt-api-backend/src/main.rs
+++ receipt-api-backend/src/main.rs
@@ -1,8 +1,18 @@
 #[macro_use] extern crate rocket;
-use std::io;
-
 use rocket::tokio::time::{sleep, Duration};
 use rocket::tokio::task::spawn_blocking;
+use std::io;
+
+// by default utilizes "FormParam"'s which is also known as a "paramater guard"
+// Rocket implements "paramater guards" for many of the standard library types, including a few special Rocket types
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello(name: &str, age: u8, cool: bool) -> String {
+	if cool {
+		format!("You're a cool {} year old, {}!", age, name)
+	} else {
+		format!("{}, we need to talk about your coolness.", name)
+	}
+}
 
 #[get("/world")]
 fn world() -> &'static str {
@@ -30,9 +40,10 @@ async fn blocker_task() -> io::Result<Vec<u8>> {
 #[launch]
 fn rocket() -> _ {
 	let my_routes = routes![
-		world,
+		blocker_task,
 		delay,
-		blocker_task
+		hello,
+		world,
 	];
 
   rocket::build().mount("/", my_routes)
