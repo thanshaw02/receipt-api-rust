b764c3817229b77c245df7ad5fae3ad8e895f06c
Tylor Hanshaw <tylorjhanshaw@gmail.com>
2023-03-12 16:05:54 -0400

    segment guard testing, learning more about Rocket

--- receipt-api-backend/src/main.rs
+++ receipt-api-backend/src/main.rs
@@ -1,9 +1,27 @@
 #[macro_use] extern crate rocket;
+use rocket::fs::NamedFile;
 use rocket::tokio::time::{sleep, Duration};
 use rocket::tokio::task::spawn_blocking;
+use std::path::{PathBuf, Path};
 use std::io;
 
-// by default utilizes "FormParam"'s which is also known as a "paramater guard"
+// we can also match against multiple segments in a path using <param..> in the routing path
+// this paramater type is known as "segment guards" and MUST implement "FromSegments"
+// segment guards but be the final component of a route's path, any text or single segments after a segment guard will throw a compile-time error
+#[get("/page/<path..>")] // note the name given to the segment guard can be anything, but must match the argument name in the corrosponding handler
+fn get_page(path: PathBuf) { /* ... */ }
+
+// thjis example serves a safe and secure static file server
+// although it's recommended to do this instead if i want/need to server a static file server in the #[launch] handler:
+/**
+ *  rocket.mount("/public", FileServer::from("static/"))
+ */
+#[get("/serve/<file..>")]
+async fn serve_page(file: PathBuf) -> Option<NamedFile> {
+	NamedFile::open(Path::new("static/").join(file)).await.ok()
+}
+
+// by default utilizes "FromParam"'s which is also known as a "paramater guard"
 // Rocket implements "paramater guards" for many of the standard library types, including a few special Rocket types
 #[get("/hello/<name>/<age>/<cool>")]
 fn hello(name: &str, age: u8, cool: bool) -> String {
